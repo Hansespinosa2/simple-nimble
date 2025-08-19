541063db279768f5650222bd8e7db4406eca3e4e
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-08-19 17:28:34 -0400

    Add 2 character seeds and change form appearance

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Character.create(
+  name: "Gorn",
+  race: "Orc",
+  nimble_class: "Zephyr",
+  level: 5,
+  background: "Angry",
+  description: "Gorn is a fierce orc Zephyr, known for his swift movements and relentless aggression. His imposing presence and quick temper make him a formidable opponent on the battlefield.",
+  languages: "Common, Orcish"
+)
+
+Character.create(
+  name: 'Luna Banana-Hammock',
+  race: "Birdfolk",
+  nimble_class: "Stormweaver",
+  level: 10,
+  background: "Inspirational leader",
+  description: "Luna is the lone survivor of an ancient birdfolk civilization, carrying the wisdom and sorrow of her lost people. Her resilience and leadership inspire those around her, and her mastery of storm magic reflects the enduring spirit of her heritage.",
+  languages: "Common, Bird, Elvish"
+)
